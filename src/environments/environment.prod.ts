42547e46872c8937f1fe59327369319c5da92992
Dirk Luijk <mail@dirkluijk.nl>
2018-09-25 00:00:22 +0200

    Github Pages build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCeoKSEWQrtHAVx7FEW2zaqNjivd7XfW4I',
+    authDomain: 'ng-chat-41dd7.firebaseapp.com',
+    databaseURL: 'https://ng-chat-41dd7.firebaseio.com',
+    projectId: 'ng-chat-41dd7',
+    storageBucket: 'ng-chat-41dd7.appspot.com',
+    messagingSenderId: '386200232055'
+  }
 };
